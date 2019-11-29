f4eda5c4473c42ca6fd52359eae6a0cbae972fb3
xstupi00 <xstupi00@stud.fit.vutbr.cz>
2019-11-29 15:32:10 +0100

    Remove trial select after the connection to the DB

--- src/main/java/org/db/InitDB.java
+++ src/main/java/org/db/InitDB.java
@@ -6,9 +6,7 @@ import oracle.ord.im.OrdImage;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class InitDB {
 
@@ -28,43 +26,22 @@ public class InitDB {
              */
             ods.setUser(System.getProperty("login"));
             ods.setPassword(System.getProperty("password"));
-            /**
-             *
-             */
 
             saved_images_to_db(ods);
-
-            // connect to the database
-            try (Connection conn = ods.getConnection()) {
-                // create a Statement
-                try (Statement stmt = conn.createStatement()) {
-                    // select something from the system's dual table
-                    try (ResultSet rset = stmt.executeQuery(
-                            "select 1+2 as col1, 3-4 as col2 from dual")) {
-                        // iterate through the result and print the values
-                        while (rset.next()) {
-                            System.out.println("col1: '" + rset.getString(1)
-                                    + "'\tcol2: '" + rset.getString(2) + "'");
-                        }
-                    } // close the ResultSet
-                } // close the Statement
-            } // close the connection
         } catch (SQLException sqlEx) {
             System.err.println("SQLException: " + sqlEx.getMessage());
         }
-
     }
 
     private static void saved_images_to_db(OracleDataSource ods) {
         // connect to the database
         File images_dir = new File("./images/");
         File[] image_name_list = images_dir.listFiles();
-        int image_id = 0;
         if (image_name_list != null) {
             try (Connection conn = ods.getConnection()) {
                 // save images to database
                 for (File image_name : image_name_list) {
-                    image_id = Image.save_image_from_file_to_db(conn, 0, image_name.getPath());
+                    Image.save_image_from_file_to_db(conn, 0, image_name.getPath());
                 }
                 System.out.println("*** SAVED IMAGES DONE ***");
 
@@ -79,11 +56,11 @@ public class InitDB {
                 Image.process_image_in_db(conn, 3, "scale", 2.25, 0.0, 0.0, 0.0);
                 Image.process_image_in_db(conn, 3, "monochrome", 0.0, 0.0, 0.0, 0.0);
 
-                // load most similiar imags from database
+                // load most similar images from database
                 int sim_image_id = Image.find_most_similar_image(conn, 3, 0.3, 0.3, 0.1, 0.3);
                 OrdImage load_image = Image.load_image_from_db(conn, sim_image_id);
                 load_image.getDataInFile("./src/load_image.gif");
-                System.out.println("*** LOAD IMAGE DONE ***");
+                System.out.println("*** LOAD SIMILAR IMAGE DONE ***");
 
             } catch (SQLException | Image.NotFoundException | IOException sqlEx) {
                 System.err.println("SQLException: " + sqlEx.getMessage());
