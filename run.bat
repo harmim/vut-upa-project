5a2929fd681e8b087791f5f96d83b9f53d8c1bfb
Dominik Harmim <harmim6@gmail.com>
2019-12-15 02:11:21 +0100

    Removed unnecessary dependencies

--- run.bat
+++ run.bat
@@ -1,7 +1,3 @@
 @echo off
 
-set /P ORACLE_USER=Oracle database username: 
-
-set /P ORACLE_PASS=Oracle database password: 
-
-call .\gradlew.bat -Dlogin=%ORACLE_USER% -Dpassword=%ORACLE_PASS% run
+call .\gradlew.bat run
